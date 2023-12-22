f53e46750fdc2e8fe6fcbd61175862fbb015b4b6
Cherrine <66070031@kmitl.ac.th>
2023-12-22 10:16:35 +0700

    Update 06-Elevetor.py

--- lab 01/06-Elevetor.py
+++ lab 01/06-Elevetor.py
@@ -5,7 +5,16 @@ class Elevator:
         self.max_floor = max_floor
     
     def go_to_floor(self, floor):
-        ...
+        max_floor = int(input())
+        floor = int(input)
+        while True:
+            if floor <= max_floor:
+                pass
+                current_floor = floor
+            elif floor > max_floor:
+                print("Invalid Floor")
+            elif floor == "Done":
+                print()
     
     def report_current_floor(self):
         print(...)
